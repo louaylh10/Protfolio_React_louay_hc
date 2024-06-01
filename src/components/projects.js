d76e3d8404a15438202afec95097e66170d2f024
louaylh10 <hcinlouay@gmail.com>
2024-06-01 14:35:42 +0100

    prtfl after add lim.tn

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,14 +1,21 @@
 
 import React from 'react';
-
+import lim from '../img/lim.png';
 const Projects = ({ projectsRef }) => {
   return (
     <div className="projects" ref={projectsRef}>
 <h2>Projects</h2>
-<ul className='project'>
-  <li>Website Builder</li>
-  <li>E-commerce shop</li>
-</ul>
+<div className='prjcts'>
+  <div class="prj">
+    <div className='wbs'>
+<img src={lim} alt='' />
+    </div>
+    <div className='prtit'>
+    <a href='http://www.lim.tn' target='_blank'>lim.tn</a>
+    </div>
+  </div>
+  
+</div>
  </div>
   );
 };
